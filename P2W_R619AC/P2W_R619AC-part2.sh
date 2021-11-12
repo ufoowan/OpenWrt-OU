b759fa71c81125b3c4dba8157e55e19043fe7b24
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-11-12 15:52:11 +0800

    Add files via upload

---
+++ P2W_R619AC/P2W_R619AC-part2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Extra package
+#git clone https://github.com/kenzok8/small.git package/small
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i "s/hostname='OpenWrt'/hostname='P2W-R619AC'/g" package/base-files/files/bin/config_generate
+
+# Modify the version number
+sed -i "s/OpenWrt /LCsub build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
