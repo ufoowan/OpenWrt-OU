6655d3ece7add77a83eab8664015c533eefa26f9
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-08 15:32:09 +0800

    Add files via upload

---
+++ Linksys-EA8500/customize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i "s/hostname='OpenWrt'/hostname='Linksys-EA8500'/g" package/base-files/files/bin/config_generate
+
+# Modify build number
+sed -i "s/OpenWrt /lc-sub build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
