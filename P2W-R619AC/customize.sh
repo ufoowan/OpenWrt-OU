46a478731848a15ffa70e88b845034f8bbda4f0a
lc-sub <92238067+lc-sub@users.noreply.github.com>
2022-02-06 16:41:01 +0800

    Update customize.sh

--- P2W-R619AC/customize.sh
+++ P2W-R619AC/customize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.14/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i "s/hostname='OpenWrt'/hostname='P2W-R619AC'/g" package/base-files/files/bin/config_generate
